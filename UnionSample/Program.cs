1dd1d56d17892d1efe38ce77eeaab2ed593b3da4
Sriram <sriram06sept@gmail.com>
2014-12-28 01:50:12 +0400

    Initial commit

---
+++ UnionSample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnionSample
+{
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Union union = new Union
+        {
+            Builder = new StringBuilder()
+        };
+
+        //text is not a string, it is StringBuilder. What the heck??
+        string text = union.Text;
+
+        //Prints some number, when I run it happens to be 25306168
+        Console.WriteLine(text.Length);
+
+        //GetType returns System.Text.StringBuilder correctly because .net 
+        //stores metadata about object in itself.
+        Console.WriteLine(text.GetType());
+
+        Console.ReadLine();
+    }
+}
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Union
+    {
+        [FieldOffset(0)]
+        public StringBuilder Builder;
+
+        [FieldOffset(0)]
+        public string Text;
+    }
+}
