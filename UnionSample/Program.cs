2a052e0b99062db628c915871598a14af364d89a
Sriram <sriram06sept@gmail.com>
2015-01-10 17:15:35 +0400

    Union struct given its own file

--- UnionSample/Program.cs
+++ UnionSample/Program.cs
@@ -1,42 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace UnionSample
 {
-internal class Program
-{
-    private static void Main(string[] args)
+    internal class Program
     {
-        Union union = new Union
+        private static void Main(string[] args)
         {
-            Builder = new StringBuilder()
-        };
+            Union union = new Union
+            {
+                Builder = new StringBuilder()
+            };
 
-        //text is not a string, it is StringBuilder. What the heck??
-        string text = union.Text;
+            //text is not a string, it is StringBuilder. What the heck??
+            string text = union.Text;
 
-        //Prints some number, when I run it happens to be 25306168
-        Console.WriteLine(text.Length);
+            //Prints some number, when I run it happens to be 25306168
+            Console.WriteLine(text.Length);
 
-        //GetType returns System.Text.StringBuilder correctly because .net 
-        //stores metadata about object in itself.
-        Console.WriteLine(text.GetType());
+            //GetType returns System.Text.StringBuilder correctly because .net 
+            //stores metadata about object in itself.
+            Console.WriteLine(text.GetType());
 
-        Console.ReadLine();
-    }
-}
-
-    [StructLayout(LayoutKind.Explicit)]
-    public struct Union
-    {
-        [FieldOffset(0)]
-        public StringBuilder Builder;
-
-        [FieldOffset(0)]
-        public string Text;
+            Console.ReadLine();
+        }
     }
 }
---
+++ UnionSample/Union.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace UnionSample
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Union
+    {
+        [FieldOffset(0)]
+        public StringBuilder Builder;
+
+        [FieldOffset(0)]
+        public string Text;
+    }
+}
